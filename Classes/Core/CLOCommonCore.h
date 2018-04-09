661af0c04509de2c10e688d01e3d26fcf62c4573
fenghaorancc <654974034@qq.com>
2018-04-09 23:52:33 +0800

    添加 weakify  strongify 宏

--- Classes/Core/CLOCommonCore.h
+++ Classes/Core/CLOCommonCore.h
@@ -16,6 +16,9 @@ FOUNDATION_EXPORT const unsigned char CLOCommonCoreVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <CLOCommon/PublicHeader.h>
 
+#define weakify(x) autoreleasepool{} __weak __typeof__(x) x##_weak = x;
+#define strongify(x) autoreleasepool{} __strong __typeof__(x) x##_weak = x;
+
 #import <CLOCommon/CLOLogMgr.h>
 
 #import <CLOCommon/CLOPathHelper.h>
